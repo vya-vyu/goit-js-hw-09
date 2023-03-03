f416f33a655afd77c4d079c9c3dbf96668677b91
vya-vyu <115100757+vya-vyu@users.noreply.github.com>
2023-03-03 18:08:09 +0000

    Deploying to gh-pages from @ vya-vyu/goit-js-hw-09@024af19c9106ae3d9c5d8fec1de4dd298d15e57b 🚀

--- 01-color-switcher.12159bd0.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),n=document.querySelector("body"),o=null;function c(){n.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}t.addEventListener("click",(function(){o=setInterval(c,1e3)})),e.addEventListener("click",(function(){clearInterval(o)}))}();
-//# sourceMappingURL=01-color-switcher.12159bd0.js.map
