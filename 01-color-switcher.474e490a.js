ea077420dbf4271cb0d3a3d8fb9c14c26590012a
vya-vyu <115100757+vya-vyu@users.noreply.github.com>
2023-03-03 22:03:00 +0000

    Deploying to gh-pages from @ vya-vyu/goit-js-hw-09@f192ba2c7ea09826e4941706cea21f38ef3c64da 🚀

--- 01-color-switcher.474e490a.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]"),o=document.querySelector("body");let n=null;function r(){o.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}e.addEventListener("click",(()=>{n=setInterval(r,1e3)}),{once:!0}),t.addEventListener("click",(()=>{clearInterval(n)}));
-//# sourceMappingURL=01-color-switcher.474e490a.js.map
