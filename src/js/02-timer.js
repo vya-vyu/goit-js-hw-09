d50a9159cedc48eab360b82c8a9d69beb7beb7b4
vya-vyu <linda1307@gmail.com>
2023-03-05 14:05:21 +0000

    task2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -24,7 +24,7 @@ const options = {
     onClose(selectedDates) {
         const currentDate = new Date();
         
-            if (currentDate >= selectedDates[0]) {
+            if (currentDate >= selectedDates[0]&& startTimerBtn.disabled === false) {
             Notiflix.Notify.failure('Please choose a date in the future');
             startTimerBtn.disabled = true;
             return;
